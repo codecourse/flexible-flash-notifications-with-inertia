f4c7cb1b7712ef353c46f6d04c214691978b643b
Alex Garrett-Smith <alex@codecourse.com>
2023-11-27 15:59:35 +0000

    All code

---
+++ resources/js/app.js
@@ -0,0 +1,28 @@
+import './bootstrap';
+import '../css/app.css';
+import "vue-toastification/dist/index.css"
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { notifications } from './Plugins/notifications';
+import Toast from 'vue-toastification'
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .use(Toast)
+            .use(notifications)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
